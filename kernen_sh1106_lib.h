9068f68ad7bcd66ff5441d759ed5ebf9287fee99
Kjell Kernen <kjell.kernen@banqit.com>
2017-03-23 23:21:08 +0100

    Auto format applied

--- kernen_sh1106_lib.h
+++ kernen_sh1106_lib.h
@@ -1,44 +1,44 @@
 /* Original copyright message that is wrong,  but may not be removed:
 **********************************************************************
-This is a library for our Monochrome OLEDs based on SSD1306 drivers
+  This is a library for our Monochrome OLEDs based on SSD1306 drivers
 
   Pick one up today in the adafruit shop!
   ------> http://www.adafruit.com/category/63_98
 
-These displays use SPI to communicate, 4 or 5 pins are required to  
-interface
+  These displays use SPI to communicate, 4 or 5 pins are required to
+  interface
 
-Adafruit invests time and resources providing this open source code, 
-please support Adafruit and open-source hardware by purchasing 
-products from Adafruit!
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
 
-Written by Limor Fried/Ladyada  for Adafruit Industries.  
-BSD license, check license.txt for more information
-All text above, and the splash screen below must be included in any
-redistribution
+  Written by Limor Fried/Ladyada  for Adafruit Industries.
+  BSD license, check license.txt for more information
+  All text above, and the splash screen below must be included in any
+  redistribution
 *********************************************************************
-WonHo-maker adapted the library in 2015 to the SH1106 displays. 
-The only major change is the display() method.
-Several scroll-related functions are not supported by the hardware,
-so he removed those.
+  WonHo-maker adapted the library in 2015 to the SH1106 displays.
+  The only major change is the display() method.
+  Several scroll-related functions are not supported by the hardware,
+  so he removed those.
 *********************************************************************
-Dvogonen (kjell kernen) bugfixed and documented in 2017.
+  Dvogonen (kjell kernen) bugfixed and documented in 2017.
 *********************************************************************/
 
 #if ARDUINO >= 100
- #include "Arduino.h"
- #define WIRE_WRITE Wire.write
+#include "Arduino.h"
+#define WIRE_WRITE Wire.write
 #else
- #include "WProgram.h"
-  #define WIRE_WRITE Wire.send
+#include "WProgram.h"
+#define WIRE_WRITE Wire.send
 #endif
 
 #ifdef __SAM3X8E__
- typedef volatile RwReg PortReg;
- typedef uint32_t PortMask;
+typedef volatile RwReg PortReg;
+typedef uint32_t PortMask;
 #else
-  typedef volatile uint8_t PortReg;
-  typedef uint8_t PortMask;
+typedef volatile uint8_t PortReg;
+typedef uint8_t PortMask;
 #endif
 
 #include <SPI.h>
@@ -66,67 +66,67 @@ Dvogonen (kjell kernen) bugfixed and documented in 2017.
     SH1106_96_16
 
     -----------------------------------------------------------------------*/
-   #define SH1106_128_64
+#define SH1106_128_64
 //   #define SH1106_128_32
 //   #define SH1106_96_16
 /*=========================================================================*/
 
 #if defined SH1106_128_64 && defined SH1106_128_32
-  #error "Only one SH1106 display can be specified at once in SH1106.h"
+#error "Only one SH1106 display can be specified at once in SH1106.h"
 #endif
 #if !defined SH1106_128_64 && !defined SH1106_128_32 && !defined SH1106_96_16
-  #error "At least one SH1106 display must be specified in SH1106.h"
+#error "At least one SH1106 display must be specified in SH1106.h"
 #endif
 
 #if defined SH1106_128_64
-  #define SH1106_LCDWIDTH		128
-  #define SH1106_LCDHEIGHT      64
+#define SH1106_LCDWIDTH		    128
+#define SH1106_LCDHEIGHT      64
 #endif
 #if defined SH1106_128_32
-  #define SH1106_LCDWIDTH       128
-  #define SH1106_LCDHEIGHT      32
+#define SH1106_LCDWIDTH       128
+#define SH1106_LCDHEIGHT      32
 #endif
 #if defined SH1106_96_16
-  #define SH1106_LCDWIDTH       96
-  #define SH1106_LCDHEIGHT      16
+#define SH1106_LCDWIDTH       96
+#define SH1106_LCDHEIGHT      16
 #endif
 
-#define SH1106_SETCONTRAST 0x81
-#define SH1106_DISPLAYALLON_RESUME 0xA4
-#define SH1106_DISPLAYALLON 0xA5
-#define SH1106_NORMALDISPLAY 0xA6
-#define SH1106_INVERTDISPLAY 0xA7
-#define SH1106_DISPLAYOFF 0xAE
-#define SH1106_DISPLAYON 0xAF
+#define SH1106_SETCONTRAST          0x81
+#define SH1106_DISPLAYALLON_RESUME  0xA4
+#define SH1106_DISPLAYALLON         0xA5
+#define SH1106_NORMALDISPLAY        0xA6
+#define SH1106_INVERTDISPLAY        0xA7
+#define SH1106_DISPLAYOFF           0xAE
+#define SH1106_DISPLAYON            0xAF
 
-#define SH1106_SETDISPLAYOFFSET 0xD3
-#define SH1106_SETCOMPINS 0xDA
+#define SH1106_SETDISPLAYOFFSET     0xD3
+#define SH1106_SETCOMPINS           0xDA
 
-#define SH1106_SETVCOMDETECT 0xDB
+#define SH1106_SETVCOMDETECT        0xDB
 
-#define SH1106_SETDISPLAYCLOCKDIV 0xD5
-#define SH1106_SETPRECHARGE 0xD9
+#define SH1106_SETDISPLAYCLOCKDIV   0xD5
+#define SH1106_SETPRECHARGE         0xD9
 
-#define SH1106_SETMULTIPLEX 0xA8
+#define SH1106_SETMULTIPLEX         0xA8
 
-#define SH1106_SETLOWCOLUMN 0x00
-#define SH1106_SETHIGHCOLUMN 0x10
+#define SH1106_SETLOWCOLUMN         0x00
+#define SH1106_SETHIGHCOLUMN        0x10
 
-#define SH1106_SETSTARTLINE 0x40
+#define SH1106_SETSTARTLINE         0x40
 
-#define SH1106_MEMORYMODE 0x20
-#define SH1106_COLUMNADDR 0x21
-#define SH1106_PAGEADDR   0x22
+#define SH1106_MEMORYMODE           0x20
+#define SH1106_COLUMNADDR           0x21
+#define SH1106_PAGEADDR             0x22
 
-#define SH1106_COMSCANINC 0xC0
-#define SH1106_COMSCANDEC 0xC8
+#define SH1106_COMSCANINC           0xC0
+#define SH1106_COMSCANDEC           0xC8
 
-#define SH1106_SEGREMAP 0xA0
+#define SH1106_SEGREMAP             0xA0
 
-#define SH1106_CHARGEPUMP 0x8D
+#define SH1106_CHARGEPUMP           0x8D
 
-#define SH1106_EXTERNALVCC 0x1
-#define SH1106_SWITCHCAPVCC 0x2
+#define SH1106_EXTERNALVCC          0x1
+#define SH1106_SWITCHCAPVCC         0x2
 
 // Scrolling #defines
 #define SH1106_ACTIVATE_SCROLL 0x2F
@@ -138,32 +138,31 @@ Dvogonen (kjell kernen) bugfixed and documented in 2017.
 #define SH1106_VERTICAL_AND_LEFT_HORIZONTAL_SCROLL 0x2A
 
 class kernen_sh1106_lib : public Adafruit_GFX {
- public:
-  kernen_sh1106_lib(int8_t SID, int8_t SCLK, int8_t DC, int8_t RST, int8_t CS);
-  kernen_sh1106_lib(int8_t DC, int8_t RST, int8_t CS);
-  kernen_sh1106_lib(int8_t RST);
+  public:
+    kernen_sh1106_lib(int8_t SID, int8_t SCLK, int8_t DC, int8_t RST, int8_t CS);
+    kernen_sh1106_lib(int8_t DC, int8_t RST, int8_t CS);
+    kernen_sh1106_lib(int8_t RST);
 
-  void begin(uint8_t switchvcc = SH1106_SWITCHCAPVCC, uint8_t i2caddr = SH1106_I2C_ADDRESS, bool reset=true);
-  void SH1106_command(uint8_t c);
-  void SH1106_data(uint8_t c);
-  void clearDisplay(void);
-  void invertDisplay(uint8_t i);
-  void display();
-  void dim(uint8_t contrast);
-  void drawPixel(int16_t x, int16_t y, uint16_t color);
+    void begin(uint8_t switchvcc = SH1106_SWITCHCAPVCC, uint8_t i2caddr = SH1106_I2C_ADDRESS, bool reset = true);
+    void SH1106_command(uint8_t c);
+    void SH1106_data(uint8_t c);
+    void clearDisplay(void);
+    void invertDisplay(uint8_t i);
+    void display();
+    void dim(uint8_t contrast);
+    void drawPixel(int16_t x, int16_t y, uint16_t color);
 
-  virtual void drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color);
-  virtual void drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color);
+    virtual void drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color);
+    virtual void drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color);
 
- private:
-  int8_t _i2caddr, _vccstate, sid, sclk, dc, rst, cs;
-  void fastSPIwrite(uint8_t c);
+  private:
+    int8_t _i2caddr, _vccstate, sid, sclk, dc, rst, cs;
+    void fastSPIwrite(uint8_t c);
 
-  boolean hwSPI;
-  PortReg *mosiport, *clkport, *csport, *dcport;
-  PortMask mosipinmask, clkpinmask, cspinmask, dcpinmask;
+    boolean hwSPI;
+    PortReg *mosiport, *clkport, *csport, *dcport;
+    PortMask mosipinmask, clkpinmask, cspinmask, dcpinmask;
 
-  inline void drawFastVLineInternal(int16_t x, int16_t y, int16_t h, uint16_t color) __attribute__((always_inline));
-  inline void drawFastHLineInternal(int16_t x, int16_t y, int16_t w, uint16_t color) __attribute__((always_inline));
-
+    inline void drawFastVLineInternal(int16_t x, int16_t y, int16_t h, uint16_t color) __attribute__((always_inline));
+    inline void drawFastHLineInternal(int16_t x, int16_t y, int16_t w, uint16_t color) __attribute__((always_inline));
 };
